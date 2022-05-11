e49924a30bdac47993de687fd2050c657e3a8856
CodeWater <1746559397@qq.com>
2022-05-11 23:55:50 +0800

    finish 52: upload

--- src/main/java/codewater/reggie/controller/CommonController.java
+++ src/main/java/codewater/reggie/controller/CommonController.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * @author ： CodeWater
@@ -24,7 +25,7 @@ import java.io.IOException;
 public class CommonController {
 
 //    动态指定文件上传的路径
-    @Value("${reggie.path")
+    @Value("${reggie.path}")
     private String basePath;
     
     /**
@@ -37,14 +38,32 @@ public class CommonController {
 // file是一个临时文件，暂时存放在："C:\Users\十二书生\AppData\Local\Temp\tomcat.8080.71757113686606579\work\Tomcat\localhost\ROOT\"目录下
 //  在下面的log那打一个端点可以查看到。所以需要转存，不然应用停止运行后就会消失
         log.info( file.toString() );
-        log.info( "{}" , basePath );
+        
+         //原始文件名  (但是可能会存在重名的问题)
+        String originalFilename = file.getOriginalFilename();
+//        截取原始文件的后缀名
+        String suffix = originalFilename.substring( originalFilename.lastIndexOf( "." ) );
+
+        //使用UUID重新生成文件名，防止文件名称重复造成文件覆盖
+        String fileName = UUID.randomUUID().toString() + suffix ;
+        
+//        创建一个目录对象
+        File dir = new File( basePath );
+//        判断当前目录是否存在
+        if( !dir.exists() ){
+            //目录不存在，需要创建
+            dir.mkdirs();
+        }
+        
         try{
-//            将临时文件转存到指定位置
-            file.transferTo( new File( basePath + "hello.jpg" ));
+//            将临时文件转存到指定位置(原视频中没有+ "/"成功运行，但是我没成功，加了之后才能正确上传)
+//            破案：配置文件少写个/
+            file.transferTo( new File( basePath  + fileName ) );
+            
         }catch(IOException e){
             e.printStackTrace();
         }
         
-        return null;
+        return R.success( fileName );
     }
 }
